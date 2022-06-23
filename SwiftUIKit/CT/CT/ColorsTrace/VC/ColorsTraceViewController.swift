3eff5171724136b670554a1db95d8faf9affe9e0
Tri1pavel <triodinp@gmail.com>
2022-06-23 12:31:33 +0300

    [swiftUIKit] [task 1] Create initial project / Add 4 buttons (with different color) to the bottom of the view.

---
+++ SwiftUIKit/CT/CT/ColorsTrace/VC/ColorsTraceViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CT
+//
+//  Created by Development on 23.06.2022.
+//
+
+import UIKit
+
+class ColorsTraceViewController: UIViewController {
+
+    @IBOutlet var colorButtons: [UIButton]!
+    
+    @IBAction func buttonWasSelected(_ sender: UIButton) {
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+

145d9b493d200a5fbe7a502e8c75e218951fc7eb
Tri1pavel <triodinp@gmail.com>
2022-06-23 16:13:45 +0300

    [swiftUIKit] [task 5] Add text on each small view - how many times has a view been created with selected color. (for each specific color).

--- SwiftUIKit/CT/CT/ColorsTrace/Model/ColorsTraceHandler.swift
+++ SwiftUIKit/CT/CT/ColorsTrace/Model/ColorsTraceHandler.swift
@@ -11,12 +11,14 @@ import UIKit
 class ColorsTraceHandler {
     private var canvas: UIView
     private var colors: [UIButton]
+    private var colorWasChangedCompletionHandler: (UIColor?) -> ()
+    
     private var selectedColor: UIColor? {
         willSet {
             colorWasChangedCompletionHandler(newValue)
         }
     }
-    private var colorWasChangedCompletionHandler: (UIColor?) -> ()
+    private var hash: [UIColor? : Int] = [:]
     
     init(canvas: UIView,
          colors: [UIButton],
@@ -29,18 +31,39 @@ class ColorsTraceHandler {
     }
     
     private func addTapGestureRecognizerToCanvas() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(addCircle(gesture:)))
+        let tap = UITapGestureRecognizer(target: self, action: #selector(addView(gesture:)))
         self.canvas.addGestureRecognizer(tap)
     }
     
     @objc
-    private func addCircle(gesture: UITapGestureRecognizer) {
+    private func addView(gesture: UITapGestureRecognizer) {
+        guard selectedColor != nil  else { return }
+        // Update hash for selected color
+        self.updateHash(ascending: true)
+        // Get location on canvas
         let location = gesture.location(in: self.canvas)
         let size = 50.0
-        let circle = UIView(frame: CGRect.init(x: location.x - size * 0.5, y: location.y - size * 0.5, width: size, height: size))
-        circle.layer.cornerRadius = size * 0.5
-        circle.backgroundColor = selectedColor
-        self.canvas.addSubview(circle)
+        let view = UILabel(frame: CGRect.init(x: location.x - size * 0.5, y: location.y - size * 0.5, width: size, height: size))
+        view.backgroundColor = selectedColor
+        // Add rounded corners
+        view.layer.cornerRadius = size * 0.5
+        view.clipsToBounds = true
+        // Set text attributes
+        view.textColor = .white
+        view.textAlignment = .center
+        view.text = String(hash[selectedColor] ?? 0)
+        // Set font
+        view.font = UIFont.boldSystemFont(ofSize: 20)
+        // Add to canvas
+        self.canvas.addSubview(view)
+    }
+    
+    private func updateHash(ascending: Bool) {
+        let current = hash[selectedColor] ?? 0
+        hash[selectedColor] =
+            ascending ?
+                current + 1 :
+                max(0, current - 1)
     }
     
 }

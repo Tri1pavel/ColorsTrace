6f24d3860b55b10b3739a163ce1fdb41d0ffe556
Tri1pavel <triodinp@gmail.com>
2022-06-23 12:39:22 +0300

    [swiftUIKit] [task 2] When tap on the button it's become selected. (last buttons become deselected).

--- SwiftUIKit/CT/CT/ColorsTrace/VC/ColorsTraceViewController.swift
+++ SwiftUIKit/CT/CT/ColorsTrace/VC/ColorsTraceViewController.swift
@@ -11,8 +11,17 @@ class ColorsTraceViewController: UIViewController {
 
     @IBOutlet var colorButtons: [UIButton]!
     
-    @IBAction func buttonWasSelected(_ sender: UIButton) {
-
+    @IBAction func buttonWasTapped(_ sender: UIButton) {
+        guard !sender.isSelected else {
+            // Deselect button that previously was selected
+            sender.isSelected.toggle()
+            return
+        }
+        
+        // Deselect all buttons
+        colorButtons.forEach { $0.isSelected = false }
+        // Select the button that was tapped
+        sender.isSelected.toggle()
     }
     
     override func viewDidLoad() {

a90a157d280d7eb9c6e5510457649a85ff9980c0
Tri1pavel <triodinp@gmail.com>
2022-06-24 11:42:52 +0300

    [Objective-C] [task 2] When tap on the button it's become selected. (last buttons become deselected).

--- Objective-C/CT/CT/Colors Trace/VC/ColorsTraceViewController.m
+++ Objective-C/CT/CT/Colors Trace/VC/ColorsTraceViewController.m
@@ -9,7 +9,7 @@
 
 @interface ColorsTraceViewController ()
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *colorButtons;
-
+@property (strong, nonatomic) UIColor *selectedColor;
 @end
 
 @implementation ColorsTraceViewController
@@ -20,7 +20,19 @@
 }
 
 - (IBAction)buttonWasTapped:(UIButton *)sender {
-    
+    if (sender.isSelected) {
+        // Deselect button that previously was selected
+        sender.selected = !sender.selected;
+    } else {
+        // Deselect all buttons
+        for (id color in self.colorButtons) {
+            [color setSelected:false];
+        }
+        // Select the button that was tapped
+        sender.selected = !sender.selected;
+    }
+
+    self.selectedColor = sender.isSelected ? sender.backgroundColor : nil;
 }
 
 @end
